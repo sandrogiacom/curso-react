1696fee69a6e078393a54facabd2a118aaf11b97
Sandro Giacomozzi <sandro.giacomozzi@grupoboticario.com.br>
2024-11-10 10:12:56 -0300

    secretword

---
+++ secretword1/src/App.js
@@ -0,0 +1,118 @@
+import "./App.css";
+
+import StartScreen from "./components/StartScreen";
+
+import { useCallback, useEffect, useState } from "react";
+import { wordsList } from "./data/words.js";
+import Game from "./components/Game";
+import GameOver from "./components/GameOver";
+
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
+];
+
+function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList);
+
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState([]);
+
+  const [guessedLetters, setGuessedLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+  const [guesses, setGuesses] = useState(3);
+  const [score, setScore] = useState(50);
+
+  const pickWordAndCategory = useCallback(() => {
+    const categories = Object.keys(words);
+    const category =
+      categories[Math.floor(Math.random() * Object.keys(categories).length)];
+
+    const word =
+      words[category][Math.floor(Math.random() * words[category].length)];
+    return { word, category };
+  }, [words]);
+
+  const startGame = useCallback(() => {
+    clearLetterStates();
+
+    const { word, category } = pickWordAndCategory();
+    let wordLetters = word.split("");
+    wordLetters = wordLetters.map((letter) => letter.toLowerCase());
+
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLetters(wordLetters);
+    setGameStage(stages[1].name);
+  }, [pickWordAndCategory]);
+
+  const verifyLetter = (letter) => {
+    const normalizedLetter = letter.toLowerCase();
+    if (
+      guessedLetters.includes(normalizedLetter) ||
+      wrongLetters.includes(normalizedLetter)
+    ) {
+      return;
+    }
+    if (letters.includes(normalizedLetter)) {
+      setGuessedLetters([...guessedLetters, normalizedLetter]);
+    } else {
+      setWrongLetters([...wrongLetters, normalizedLetter]);
+      setGuesses(guesses - 1);
+    }
+  };
+
+  const clearLetterStates = () => {
+    setGuessedLetters([]);
+    setWrongLetters([]);
+  };
+
+  useEffect(() => {
+    if (guesses <= 0) {
+      //reset all states
+      clearLetterStates();
+      setGameStage(stages[2].name);
+    }
+  }, [guesses]);
+
+  useEffect(() => {
+    if (guessedLetters.length === letters.length) {
+      const uniqueLetters = [...new Set(letters)];
+      //win condition
+      if (uniqueLetters.length === guessedLetters.length) {
+        setScore((actualScore) => actualScore + 100);
+        startGame();
+      }
+    }
+  }, [guessedLetters, letters, startGame]);
+
+  const retry = () => {
+    setScore(0);
+    setGuesses(3);
+    setGameStage(stages[0].name);
+  };
+
+  return (
+    <div className="App">
+      {gameStage === "start" && <StartScreen startGame={startGame} />}
+      {gameStage === "game" && (
+        <Game
+          verifyLetter={verifyLetter}
+          pickedWord={pickedWord}
+          pickedCategory={pickedCategory}
+          letters={letters}
+          guessedLetters={guessedLetters}
+          wrongLetters={wrongLetters}
+          guesses={guesses}
+          score={score}
+        />
+      )}
+      {gameStage === "end" && <GameOver retry={retry} score={score} />}
+    </div>
+  );
+}
+
+export default App;
---
+++ secretword1/src/data/words.js
@@ -0,0 +1,7 @@
+export const wordsList = {
+  alimento: ["Leite", "Ovo", "Pão", "Queijo", "Fruta", "Carne", "Arroz", "Feijão", "Macarrão", "Café", "Açúcar", "Sal", "Óleo", "Manteiga", "Chocolate"],
+  casa: ["Cama", "Sofá", "Mesa", "Cadeira", "Geladeira", "Fogão", "Pia", "Banheiro", "Quarto", "Sala", "Cozinha", "Jardim", "Porta", "Janela", "Parede"],
+  escola: ["Professor", "Aluno", "Caderno", "Lápis", "Caneta", "Livro", "Prova", "Sala de aula", "Quadro", "Carteira", "Biblioteca", "Diretor", "Recreio", "Lousa", "Mochila"],
+  shopping: ["Loja", "Roupa", "Sapato", "Cinema", "Restaurante", "Escada rolante", "Estacionamento", "Caixa eletrônico", "Promoção", "Vitrine", "Manequim", "Provador", "Segurança", "Elevador", "Perfume"],
+  hotel: ["Quarto", "Cama", "Recepção", "Restaurante", "Piscina", "Bar", "Serviço de quarto", "Café da manhã", "Toalha", "Chave", "Hóspede", "Mala", "Camareira", "Garçom", "Portaria"],
+};
